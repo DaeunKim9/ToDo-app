60f780cd6492885398351f5fb6408ba365f8007a
kimdaeun <onlydk57@gmail.com>
2022-12-16 16:16:21 +0900

    Initial commit

---
+++ src/main/java/com/codestates/Sectionprojectsolo/SectionProjectSoloApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.Sectionprojectsolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SectionProjectSoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SectionProjectSoloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/Sectionprojectsolo/SectionProjectSoloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.Sectionprojectsolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SectionProjectSoloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
