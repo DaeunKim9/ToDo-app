6c5e3dc08a001fc988b06bef55df5e9118445131
kimdaeun <onlydk57@gmail.com>
2022-12-16 16:31:28 +0900

    initial commit

--- src/main/java/com/codestates/Sectionprojectsolo/RecommendationController.java
+++ src/main/java/com/codestates/Sectionprojectsolo/RecommendationController.java
@@ -1,4 +1,15 @@
 package com.codestates.Sectionprojectsolo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RecommendationController {
+
+    @GetMapping ("/")
+    public String helloWorld() {
+        return "To-Do Application";
+    }
+
+
 }
